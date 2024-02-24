05a228c1ee01002c4442db1937fd5b14e0fca7a2
Miguel Angel <miguelangelhh17@gmail.com>
2024-02-24 16:20:15 -0500

    Fix types on API

---
+++ src/pages/api/createPlaylist.ts
@@ -0,0 +1,40 @@
+import { google } from "googleapis";
+import { promises as fs } from "fs";
+import {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECTION_URI,
+} from "../../models/credentials";
+import createPlaylist from "../../services/createPlaylist";
+
+const oAuth2Client: any = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECTION_URI
+);
+
+export default async function handler(req: any, res: any) {
+  try {
+    const {
+      newItems,
+      customPlaylistId,
+    }: { newItems: string; customPlaylistId?: string } = req.query;
+
+    const token: any = await fs.readFile("token.json");
+    const parsedToken = JSON.parse(token);
+    oAuth2Client.setCredentials(parsedToken);
+
+    const newPlaylistId = await createPlaylist({
+      auth: oAuth2Client,
+      newItems,
+      customPlaylistId,
+    });
+
+    res
+      .status(200)
+      .json({ message: "Playlist created successfully", newPlaylistId });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
